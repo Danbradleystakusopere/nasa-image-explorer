87f94e539b10bf8f55f37971bff3ffc318a745df
Danbradleystakusopere <danstakus@gmail.com>
2025-06-25 10:43:09 +0300

    Set up DOMContentLoaded and selected date input & fetch button

---
+++ Index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fetchBtn = document.getElementById("fetchBtn");
+  const dateInput = document.getElementById("dateInput");
+
+  console.log("NASA Image Explorer is ready.");
+});
